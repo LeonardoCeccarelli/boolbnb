e68093490149cfbf4542c93e945cc79d8e0345a1
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-02-10 12:39:25 +0100

    add mix.disablesuccessnotifications in webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,7 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.disableSuccessNotifications(); //Rimuove le notifiche andate a buon fine dopo aver compilato
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
