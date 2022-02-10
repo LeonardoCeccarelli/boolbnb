eec81d721e762b4c1e37ddfdfc0c997bb2e326a5
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-02-10 15:14:41 +0100

    add sponsor migration

---
+++ database/migrations/2022_02_10_134616_create_sponsor_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSponsorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsor', function (Blueprint $table) {
+            $table->id();
+            $table->string("type");
+            $table->float("price", 6, 2);
+            $table->integer("duration");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsor');
+    }
+}
