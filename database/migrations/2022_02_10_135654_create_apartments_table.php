6335af9871bf61b096377024a7a6b1fed35e09fb
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-02-10 15:14:59 +0100

    add apartments migration

---
+++ database/migrations/2022_02_10_135654_create_apartments_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->text("description");
+            $table->text("cover_img");
+            $table->integer("rooms");
+            $table->integer("beds");
+            $table->integer("bathrooms");
+            $table->integer("square_metres");
+            $table->float("night_price", 6, 2);
+            $table->text("address");
+            $table->string("city");
+            $table->string("lat");
+            $table->string("lon");
+            $table->boolean("visible");
+
+            $table->unsignedBigInteger("user_id");
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("users");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('apartments', function (Blueprint $table) {
+            $table->dropForeign('apartments_user_id_foreign');
+            $table->dropColumn('user_id');
+        });
+        Schema::dropIfExists('apartments');
+    }
+}
