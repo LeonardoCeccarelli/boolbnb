d2e5c38f97e895e3329be245b691c324d2082d37
GiovanniConti <giovanni.conti@live.it>
2022-02-11 15:20:49 +0100

    Fix icons ServicesSeeder

--- database/seeds/ServicesTableSeeder.php
+++ database/seeds/ServicesTableSeeder.php
@@ -5,63 +5,63 @@ use Illuminate\Database\Seeder;
 
 class ServicesTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        $data = [
-            [
-                "name" => "Wi-fi",
-                "icon" => '<i class="far fa-wifi"></i>',
-            ],
-            [
-                "name" => "Posto Macchina",
-                "icon" => '<i class="far fa-car"></i>',
-            ],
-            [
-                "name" => "Piscina",
-                "icon" => '<i class="far fa-swimmer"></i>',
-            ],
-            [
-                "name" => "Portineria",
-                "icon" => '<i class="fas fa-concierge-bell"></i>',
-            ],
-            [
-                "name" => "Cucina",
-                "icon" => '<i class="far fa-oven"></i>',
-            ],
-            [
-                "name" => "Sauna",
-                "icon" => '<i class="far fa-hot-tub"></i>',
-            ],
-            [
-                "name" => "Vista mare",
-                "icon" => '<i class="far fa-umbrella-beach"></i>',
-            ],
-            [
-                "name" => "Palestra",
-                "icon" => '<i class="far fa-dumbbell"></i>',
-            ],
-            [
-                "name" => "Aria condizionata",
-                "icon" => '<i class="far fa-air-conditioner"></i>',
-            ],
-            [
-                "name" => "TV",
-                "icon" => '<i class="far fa-tv"></i>',
-            ],
-        ];
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    $data = [
+      [
+        "name" => "Wi-fi",
+        "icon" => '<i class="fas fa-wifi"></i>',
+      ],
+      [
+        "name" => "Posto Macchina",
+        "icon" => '<i class="fas fa-car"></i>',
+      ],
+      [
+        "name" => "Piscina",
+        "icon" => '<i class="fas fa-swimmer"></i>',
+      ],
+      [
+        "name" => "Portineria",
+        "icon" => '<i class="fas fa-concierge-bell"></i>',
+      ],
+      [
+        "name" => "Cucina",
+        "icon" => '<i class="fas fa-utensils"></i>',
+      ],
+      [
+        "name" => "Sauna",
+        "icon" => '<i class="fas fa-hot-tub"></i>',
+      ],
+      [
+        "name" => "Vista mare",
+        "icon" => '<i class="fas fa-umbrella-beach"></i>',
+      ],
+      [
+        "name" => "Palestra",
+        "icon" => '<i class="fas fa-dumbbell"></i>',
+      ],
+      [
+        "name" => "Aria condizionata",
+        "icon" => '<i class="fas fa-wind"></i>',
+      ],
+      [
+        "name" => "TV",
+        "icon" => '<i class="fas fa-tv"></i>',
+      ],
+    ];
 
-        foreach ($data as $value) {
-            $newValue = new Service();
+    foreach ($data as $value) {
+      $newValue = new Service();
 
-            $newValue->name = $value["name"];
-            $newValue->icon = $value["icon"];
+      $newValue->name = $value["name"];
+      $newValue->icon = $value["icon"];
 
-            $newValue->save();
-        }
+      $newValue->save();
     }
+  }
 }
