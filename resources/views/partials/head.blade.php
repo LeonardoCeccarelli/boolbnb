d8144d34817e7254c68c85654c784e62c4e6a003
omar96developer <omakino@hotmail.it>
2022-02-14 22:21:58 +0100

    update layout

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -5,7 +5,7 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('page_title', 'Laravel')</title>
+  <title>@yield('page_title', 'BoolBnB')</title>
 
   <!-- Scripts -->
   <script src="{{ asset('js/app.js') }}" defer></script>
