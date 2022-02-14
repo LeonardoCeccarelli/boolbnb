495058104820c6dc351b2a9d62fb3aaeb3fa0f26
GiovanniConti <giovanni.conti@live.it>
2022-02-14 16:04:36 +0100

    Fix Api call problem and saving lat/lon on db

--- app/Http/Controllers/Admin/ApartmentController.php
+++ app/Http/Controllers/Admin/ApartmentController.php
@@ -7,142 +7,158 @@ use App\Http\Controllers\Controller;
 use App\Service;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Storage;
 
 class ApartmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        // Il metodo index è gestito dall' Admin/HomeController@index 
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function index()
+  {
+    // Il metodo index è gestito dall' Admin/HomeController@index 
+  }
+
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    $services = Service::all();
+
+    return view("admin.apartment.create", [
+      "services" => $services,
+    ]);
+  }
+
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+
+    // Chiamata APi per ricevere lat e lon
+    $address = str_replace(' ', '%20', $request['address']);
+    $city = $request['city'];
+    $key = '74G2HVlLeNW6ZnVG4yzsaMj20OxuW1sJ';
+    $baseUrl = 'https://api.tomtom.com/search/2/geocode/';
+    $endUrl = '.json?key=';
+    $url = $baseUrl . $city . '%20' . $address . $endUrl . $key;
+
+    $response = Http::get($url);
+    $lat = $response->json()['results'][0]['position']['lat'];
+    $lon = $response->json()['results'][0]['position']['lon'];
+    // Fine chiamata API
+
+    // ESEGUIRE LA VALIDAZIONE
+
+    $data = $request->all();
+    $newApartment = new Apartment();
+    $newApartment->fill($data);
+    $newApartment->visible = $request["visible"] ? "1" : "0";
+    $newApartment->lat = $lat;
+    $newApartment->lon = $lon;
+    $newApartment->user_id = Auth::user()->id;
+
+    $newApartment->save();
+
+
+
+    if (array_key_exists("services", $data)) {
+      $newApartment->services()->sync($data["services"]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $services = Service::all();
+    return redirect()->route("admin.apartment.show", $newApartment->id);
+  }
 
-        return view("admin.apartment.create", [
-            "services" => $services,
-        ]);
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show(Apartment $apartment)
+  {
+    return view("admin.apartment.show", [
+      "apartment" => $apartment,
+    ]);
+  }
+
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit(Apartment $apartment)
+  {
+
+    $services = Service::all();
+
+    return view("admin.apartment.edit", [
+      "apartment" => $apartment,
+      "services" => $services,
+    ]);
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, Apartment $apartment)
+  {
+    // ESEGUIRE VALIDAZIONE
+
+    $data = $request->all();
+
+    $oldImage = $apartment->cover_img;
+    $apartment->fill($data);
+    $apartment->visible = $request["visible"] ? "1" : "0";
+
+
+    if ($request->file("coverImg")) {
+
+      if ($oldImage) {
+        Storage::delete($oldImage);
+      }
+
+      $apartment->cover_img = $request->file("cover_img")->store("apartments");
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        // ESEGUIRE LA VALIDAZIONE
+    $apartment->save();
 
-        $data = $request->all();
-        $newApartment = new Apartment();
-
-        $newApartment->fill($data);
-
-        $newApartment->visible = $request["visible"] ? "1" : "0";
-
-        $newApartment->user_id = Auth::user()->id;
-
-        $newApartment->save();
-
-        if (array_key_exists("services", $data)) {
-            $newApartment->services()->sync($data["services"]);
-        }
-
-        return redirect()->route("admin.apartment.show", $newApartment->id);
+    if (array_key_exists("services", $data)) {
+      $apartment->services()->sync($data["services"]);
+    } else {
+      $apartment->services()->detach();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Apartment $apartment)
-    {
-        return view("admin.apartment.show", [
-            "apartment" => $apartment,
-        ]);
-    }
+    return redirect()->route("admin.apartment.show", $apartment->id);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Apartment $apartment)
-    {
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy(Apartment $apartment)
+  {
+    $apartment->services()->detach();
 
-        $services = Service::all();
+    $apartment->delete();
 
-        return view("admin.apartment.edit", [
-            "apartment" => $apartment,
-            "services" => $services,
-        ]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Apartment $apartment)
-    {
-        // ESEGUIRE VALIDAZIONE
-
-        $data = $request->all();
-
-        $oldImage = $apartment->cover_img;
-        $apartment->fill($data);
-        $apartment->visible = $request["visible"] ? "1" : "0";
-
-
-        if ($request->file("coverImg")) {
-
-            if ($oldImage) {
-                Storage::delete($oldImage);
-            }
-
-            $apartment->cover_img = $request->file("cover_img")->store("apartments");
-        }
-
-        $apartment->save();
-
-        if (array_key_exists("services", $data)) {
-            $apartment->services()->sync($data["services"]);
-        } else {
-            $apartment->services()->detach();
-        }
-
-        return redirect()->route("admin.apartment.show", $apartment->id);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Apartment $apartment)
-    {
-        $apartment->services()->detach();
-
-        $apartment->delete();
-
-        return redirect()->route("admin.home");
-    }
+    return redirect()->route("admin.home");
+  }
 }
--- resources/views/admin/apartment/create.blade.php
+++ resources/views/admin/apartment/create.blade.php
@@ -4,193 +4,171 @@
 
 @section('content')
 <div class="container">
-    <div class="mb-5 pb-5 text-center">
-        <h2>Ciao {{Auth::user()->first_name}}</h2>
-        <h4>In questa sezione puoi inserire un nuovo appartamento</h4>
+  <div class="mb-5 pb-5 text-center">
+    <h2>Ciao {{Auth::user()->first_name}}</h2>
+    <h4>In questa sezione puoi inserire un nuovo appartamento</h4>
+  </div>
+  <form id="form_submit" action="{{ route('admin.apartment.store') }}" enctype="multipart/form-data" method="POST"
+    class="row justify-content-center gy-5">
+    @csrf
+
+    {{-- Toggle Visibility --}}
+    <div class="col-8">
+      <div class="form-check form-switch">
+        <input class="form-check-input" name="visible" type="checkbox" role="switch" id="flexSwitchCheckChecked"
+          value="visible" checked>
+        <label class="form-check-label" for="flexSwitchCheckChecked">Visibile</label>
+      </div>
     </div>
-    <form id="form_submit" action="{{ route('admin.apartment.store') }}" enctype="multipart/form-data" method="POST"
-        class="row justify-content-center gy-5">
-        @csrf
 
-        <div class="col-8">
-            <div class="form-check form-switch">
-                <input class="form-check-input" name="visible" type="checkbox" role="switch" id="flexSwitchCheckChecked"
-                    value="visible" checked>
-                <label class="form-check-label" for="flexSwitchCheckChecked">Visibile</label>
-            </div>
+    {{-- Title --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label for="field_title" class="form-label">Titolo</label>
+      <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" name="title"
+        id="field_title" value="{{ old('title') }}">
+
+      @if($errors->has("title"))
+      <div class="invalid-feedback">
+        {{ $errors->get("title")[0]}}
+      </div>
+      @endif
+    </div>
+
+    {{-- Description --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label class="form-label">Descrizione</label>
+      <textarea type="text" rows="5" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}"
+        name="description">{{ old("description") }}</textarea>
+
+      @if($errors->has("description"))
+      <div class="invalid-feedback">
+        {{ $errors->get("description")[0]}}
+      </div>
+      @endif
+    </div>
+
+    {{-- Address --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label for="field_address" class="form-label">Indirizzo</label>
+      <input type="text" class="form-control {{ $errors->has('address') ? 'is-invalid' : '' }}" name="address"
+        id="field_address" value="{{ old('address') }}">
+    </div>
+
+    {{-- City --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label for="field_city" class="form-label">Città</label>
+      <input type="text" class="form-control {{ $errors->has('city') ? 'is-invalid' : '' }}" name="city" id="field_city"
+        value="{{ old('city') }}">
+    </div>
+
+    <input id="field_lat" hidden type="text" value="" name="lat">
+    <input id="field_lon" hidden type="text" value="" name="lon">
+
+    {{-- Cover_Img --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label for="field_cover_img" class="form-label">Link Immagine</label>
+      <input type="file" class="form-control {{ $errors->has('cover_img') ? 'is-invalid' : '' }}" name="cover_img"
+        id="field_cover_img" value="{{ old('cover_img') }}">
+
+      @if($errors->has("cover_img"))
+      <div class="invalid-feedback">
+        {{ $errors->get("cover_img")[0]}}
+      </div>
+      @endif
+    </div>
+
+    {{-- Rooms Number --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-5 g-3">
+        <div class="col">
+          <label for="field_rooms" class="form-label">N° Stanze</label>
+          <input type="number" min="1" max="20" class="form-control {{ $errors->has('rooms') ? 'is-invalid' : ''}}"
+            name="rooms" id="field_rooms" value="{{ old('rooms') }}">
+
+          @if($errors->has("rooms"))
+          <div class="invalid-feedback">
+            {{ $errors->get("rooms")[0]}}
+          </div>
+          @endif
         </div>
 
-        <div class="col-12 col-md-10 col-lg-8">
-            <label for="field_title" class="form-label">Titolo</label>
-            <input type="text" class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}" name="title"
-                id="field_title" value="{{ old('title') }}">
+        {{-- Beds Number --}}
+        <div class="col">
+          <label for="field_beds" class="form-label">N° Letti</label>
+          <input type="number" min="1" max="20" class="form-control {{ $errors->has('beds') ? 'is-invalid' : ''}}"
+            name="beds" id="field_beds" value="{{ old('beds') }}">
 
-            @if($errors->has("title"))
-            <div class="invalid-feedback">
-                {{ $errors->get("title")[0]}}
-            </div>
-            @endif
+          @if($errors->has("beds"))
+          <div class="invalid-feedback">
+            {{ $errors->get("beds")[0]}}
+          </div>
+          @endif
         </div>
 
-        <div class="col-12 col-md-10 col-lg-8">
-            <label class="form-label">Descrizione</label>
-            <textarea type="text" rows="5" class="form-control {{ $errors->has('description') ? 'is-invalid' : '' }}"
-                name="description">{{ old("description") }}</textarea>
+        {{-- Bathrooms Number --}}
+        <div class="col">
+          <label for="field_bathrooms" class="form-label">N° Bagni</label>
+          <input type="number" min="1" max="20" class="form-control {{ $errors->has('bathrooms') ? 'is-invalid' : ''}}"
+            name="bathrooms" id="field_bathrooms" value="{{ old('bathrooms') }}">
 
-            @if($errors->has("description"))
-            <div class="invalid-feedback">
-                {{ $errors->get("description")[0]}}
-            </div>
-            @endif
+          @if($errors->has("bathrooms"))
+          <div class="invalid-feedback">
+            {{ $errors->get("bathrooms")[0]}}
+          </div>
+          @endif
         </div>
 
-        <div class="col-12 col-md-10 col-lg-8">
-            <label for="field_address" class="form-label">Indirizzo</label>
-            <input type="text" class="form-control {{ $errors->has('address') ? 'is-invalid' : '' }}" name="address"
-                id="field_address" value="{{ old('address') }}">
+        {{-- Square_Metres --}}
+        <div class="col">
+          <label for="field_square_metres" class="form-label">Metri quadrati</label>
+          <input type="number" min="1" max="300"
+            class="form-control {{ $errors->has('square_metres') ? 'is-invalid' : ''}}" name="square_metres"
+            id="field_square_metres" value="{{ old('square_metres') }}">
+
+          @if($errors->has("square_metres"))
+          <div class="invalid-feedback">
+            {{ $errors->get("square_metres")[0]}}
+          </div>
+          @endif
         </div>
 
-        <div class="col-12 col-md-10 col-lg-8">
-            <label for="field_city" class="form-label">Città</label>
-            <input type="text" class="form-control {{ $errors->has('city') ? 'is-invalid' : '' }}" name="city"
-                id="field_city" value="{{ old('city') }}">
+        {{-- Price_per_Night --}}
+        <div class="col">
+          <label for="field_night_price" class="form-label">Prezzo per notte</label>
+          <input type="number" step="0.01" min="0" max="9999"
+            class="form-control {{ $errors->has('night_price') ? 'is-invalid' : ''}}" name="night_price"
+            id="field_night_price" value="{{ old('night_price') }}">
+
+          @if($errors->has("night_price"))
+          <div class="invalid-feedback">
+            {{ $errors->get("night_price")[0]}}
+          </div>
+          @endif
         </div>
+      </div>
+    </div>
 
-        <input id="field_lat" hidden type="text" value="" name="lat">
-        <input id="field_lon" hidden type="text" value="" name="lon">
+    {{-- Services --}}
+    <div class="col-12 col-md-10 col-lg-8">
+      <label for="field_services" class="form-label">Servizi</label>
+      <small class="text-secondary mb-0 ms-3 pt-1 d-inline-block">Hold down the Ctrl (windows) or Command
+        (Mac) button to select multiple options.</small>
+      <select multiple class="form-control" name="services[]" id="field_services">
+        @foreach ($services as $service)
+        <option value="{{ $service->id }}">{{ $service->name }}</option>
+        @endforeach
+      </select>
+    </div>
 
-        <div class="col-12 col-md-10 col-lg-8">
-            <label for="field_cover_img" class="form-label">Link Immagine</label>
-            <input type="file" class="form-control {{ $errors->has('cover_img') ? 'is-invalid' : '' }}" name="cover_img"
-                id="field_cover_img" value="{{ old('cover_img') }}">
+    <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-center">
+      {{-- STILI BUTTON DA DEFINIRE --}}
+      <button type="submit" class="btn btn-primary">Aggiungi</button>
+    </div>
 
-            @if($errors->has("cover_img"))
-            <div class="invalid-feedback">
-                {{ $errors->get("cover_img")[0]}}
-            </div>
-            @endif
-        </div>
-
-        <div class="col-12 col-md-10 col-lg-8">
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-xl-5 g-3">
-                <div class="col">
-                    <label for="field_rooms" class="form-label">N° Stanze</label>
-                    <input type="number" min="1" max="20"
-                        class="form-control {{ $errors->has('rooms') ? 'is-invalid' : ''}}" name="rooms"
-                        id="field_rooms" value="{{ old('rooms') }}">
-
-                    @if($errors->has("rooms"))
-                    <div class="invalid-feedback">
-                        {{ $errors->get("rooms")[0]}}
-                    </div>
-                    @endif
-                </div>
-
-                <div class="col">
-                    <label for="field_beds" class="form-label">N° Letti</label>
-                    <input type="number" min="1" max="20"
-                        class="form-control {{ $errors->has('beds') ? 'is-invalid' : ''}}" name="beds" id="field_beds"
-                        value="{{ old('beds') }}">
-
-                    @if($errors->has("beds"))
-                    <div class="invalid-feedback">
-                        {{ $errors->get("beds")[0]}}
-                    </div>
-                    @endif
-                </div>
-
-                <div class="col">
-                    <label for="field_bathrooms" class="form-label">N° Bagni</label>
-                    <input type="number" min="1" max="20"
-                        class="form-control {{ $errors->has('bathrooms') ? 'is-invalid' : ''}}" name="bathrooms"
-                        id="field_bathrooms" value="{{ old('bathrooms') }}">
-
-                    @if($errors->has("bathrooms"))
-                    <div class="invalid-feedback">
-                        {{ $errors->get("bathrooms")[0]}}
-                    </div>
-                    @endif
-                </div>
-
-                <div class="col">
-                    <label for="field_square_metres" class="form-label">Metri quadrati</label>
-                    <input type="number" min="1" max="300"
-                        class="form-control {{ $errors->has('square_metres') ? 'is-invalid' : ''}}" name="square_metres"
-                        id="field_square_metres" value="{{ old('square_metres') }}">
-
-                    @if($errors->has("square_metres"))
-                    <div class="invalid-feedback">
-                        {{ $errors->get("square_metres")[0]}}
-                    </div>
-                    @endif
-                </div>
-
-                <div class="col">
-                    <label for="field_night_price" class="form-label">Prezzo per notte</label>
-                    <input type="number" step="0.01" min="0" max="9999"
-                        class="form-control {{ $errors->has('night_price') ? 'is-invalid' : ''}}" name="night_price"
-                        id="field_night_price" value="{{ old('night_price') }}">
-
-                    @if($errors->has("night_price"))
-                    <div class="invalid-feedback">
-                        {{ $errors->get("night_price")[0]}}
-                    </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-
-        <div class="col-12 col-md-10 col-lg-8">
-            <label for="field_services" class="form-label">Servizi</label>
-            <small class="text-secondary mb-0 ms-3 pt-1 d-inline-block">Hold down the Ctrl (windows) or Command
-                (Mac) button to select multiple options.</small>
-            <select multiple class="form-control" name="services[]" id="field_services">
-                @foreach ($services as $service)
-                <option value="{{ $service->id }}">{{ $service->name }}</option>
-                @endforeach
-            </select>
-        </div>
-
-        <div class="col-12 col-md-10 col-lg-8 d-flex justify-content-center">
-            {{-- STILI BUTTON DA DEFINIRE --}}
-            <button type="submit" class="btn btn-primary">Aggiungi</button>
-        </div>
-
-    </form>
+  </form>
 </div>
 @endsection
 
 @section('scripts')
-<script>
-    window.addEventListener("DOMContentLoaded", () => {  
-        const address = document.getElementById('field_address')
-        const city = document.getElementById('field_city')
-        const lat = document.getElementById('field_lat')
-        const lon = document.getElementById('field_lon')
 
-        $form = document.getElementById("form_submit");
-        $form.addEventListener("submit", (e) => {    
-            e.preventDefault() 
-            window.axios.get("https://api.tomtom.com/search/2/geocode/4%20north%202nd%20street%20san%20jose.json?key=74G2HVlLeNW6ZnVG4yzsaMj20OxuW1sJ", {
-                headers: {
-                    'access-control-request-headers': '*',
-                    'access-control-allow-origin': '*',
-                }
-            }).then(resp => {
-                console.log(resp)
-            })
-            // window.axios.get("https://api.tomtom.com/search/2/geocode/" + city.value + " " + address.value + ".json", {
-            //     params: {
-            //         storeResult: "false",
-            //         typehead: "true",
-            //         extendedPostalCodesFor: "addr",
-            //         view: "Unified",
-            //         key: "74G2HVlLeNW6ZnVG4yzsaMj20OxuW1sJ",
-
-            //     }
-            // }).then((resp) => {
-            //     console.log(resp.data);
-            // })
-    })});
-</script>
 @endsection
