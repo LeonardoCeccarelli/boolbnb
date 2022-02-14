0b88502a77fceeeb6621aea594ae50f182d685be
GiovanniConti <giovanni.conti@live.it>
2022-02-14 13:30:16 +0100

    Added View Sponsor with sponsors display

--- resources/views/admin/sponsor/index.blade.php
+++ resources/views/admin/sponsor/index.blade.php
@@ -1,6 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>sezione sponsor</h1>
-    
+<h1 class="text-center">sezione sponsor</h1>
+
+<div class="container">
+  <div class="row row-cols-3">
+    @foreach ($sponsor as $single_sponsor)
+    <div class="col d-flex justify-content-center text-center">
+
+      {{-- Single Sponsor Card --}}
+      <div class="card" style="width: 18rem;">
+        <img src="" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Tipo: {{ $single_sponsor->type }}</h5>
+          <p class="card-text">Prezzo: {{ $single_sponsor->price }}</p>
+          <p class="card-text">Durata: {{ $single_sponsor->duration }}</p>
+
+          <a href="#" class="btn btn-primary">Buy</a>
+        </div>
+      </div>
+
+    </div>
+    @endforeach
+  </div>
+</div>
+
 @endsection
