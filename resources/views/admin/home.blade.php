f8cc9ad88f5a92daf85ecb543ba1eb774af29b68
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2022-02-14 09:24:10 +0100

    add link for view create

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -4,23 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-10">
-            {{-- <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <h1 class="text-center">Dashboard con lista Appartamenti per utente</h1>
-
-                <div class="card-body">
-                    @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-
-                </div>
-            </div> --}}
             <h2>Benvenuto {{ Auth::user()->first_name }} <i class="far fa-wifi"></i></h2>
+            <a href="{{ route('admin.apartment.create') }}">Crea nuovo</a>
             <div class="row row-cols-1">
                 @foreach ($apartments as $apartment)
                 <div class="col">
