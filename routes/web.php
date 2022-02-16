1f6bc7133e820ce957707a0592e8d6c147d56c4d
omar96developer <omakino@hotmail.it>
2022-02-16 10:12:20 +0100

    update 3

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::middleware("auth")
         Route::get("/", "HomeController@index")->name("home");
 
         Route::resource("/apartment", "ApartmentController");
-        Route::get("/apartment/sponsor", "SponsorController@index")->name("sponsor");
+        Route::get("/sponsor", "SponsorController@index")->name("sponsor");
     });
 
 Route::get("{any?}", function () {
