2542c5c7d9fe50fc76920e8f9630496d73df9b43
5oN1A <82112240+5oN1A@users.noreply.github.com>
2022-02-16 15:39:09 +0100

    create 3 sections in guest.welcome

--- resources/js/app.js
+++ resources/js/app.js
@@ -20,6 +20,12 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('hero', require('./components/welcome/Hero.vue').default);
+Vue.component('sponsored', require('./components/welcome/Sponsored.vue').default);
+Vue.component('cities', require('./components/welcome/Cities.vue').default);
+
+
+
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,7 +61,7 @@
       </div>
     </nav>
 
-    <main class="py-4">
+    <main>
       @yield('content')
     </main>
   </div>
