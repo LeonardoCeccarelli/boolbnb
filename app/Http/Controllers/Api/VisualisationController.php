f052faa163851bb721820422dcae0c3d503fdd7b
Giovanni Conti <89584642+GiovanniConti@users.noreply.github.com>
2022-02-22 17:09:37 +0100

    Implemented function to add a view to the db with relative checks

--- app/Http/Controllers/Api/VisualisationController.php
+++ app/Http/Controllers/Api/VisualisationController.php
@@ -17,7 +17,18 @@ class VisualisationController extends Controller
       $data = $request->all();
       $now = Carbon::now();
 
-      // dd($request->all());
+      // Take elements from db with ip_address and apartment_id same as passed in $request
+      $already_exist = Visualisation::where('ip_address', $data[1])
+                        ->where('apartment_id', $data[0])->first();
+
+      // check if exist if the elements serched before exist
+      if($already_exist){
+        $existing_date = $already_exist->created_at->addHours(12);
+        // check if the element found previously was added to the db less than 12h ago
+        if($now < $existing_date){
+          return [ "status" => "already added less than 12h ago"];
+        }
+      }
 
       $newVisualisation = new Visualisation();
       $newVisualisation->apartment_id = $data[0];
@@ -25,7 +36,6 @@ class VisualisationController extends Controller
 
       $newVisualisation->save();
 
-      return;
-
+      return ["status" => "added successfully"];
     }
 }
